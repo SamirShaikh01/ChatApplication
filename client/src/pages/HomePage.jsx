9e54e339967c7803afcfb30608f8888c46c982e3
khankabi <khankabir077777@gmail.com>
2025-10-14 00:23:56 +0530

    Sidebar Image function added

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSideBar from '../components/RightSideBar'
+import { useContext } from 'react'
+import { ChatContext } from '../context/ChatContext'
 
 function HomePage() {
-  const [selectedUser,setSelectedUser] = useState(false)
+  const {selectedUser} = useContext(ChatContext);
   
   return (
     <div>
@@ -12,8 +14,8 @@ function HomePage() {
         <div className={` backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%]
         grid grid-cols-1 relative text-white ${selectedUser ?'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' :'md:grid-cols-2' }`}>
           <Sidebar />
-          <ChatContainer  selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
-          <RightSideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+          <ChatContainer/>
+          <RightSideBar/>
         </div>
       </div>
     </div>
