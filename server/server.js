58b601e7b75b5ae45a25c46149799056d4d0bd3d
khankabi <khankabir077777@gmail.com>
2025-10-16 12:06:32 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -58,11 +58,9 @@ app.use("/api/messages", messageRouter)
 //connect to mongodb
 await connectDB();
 
-if (process.env.NODE_ENV !== "production"){
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT, () => {
-        console.log(`Server is running on: http://localhost:${PORT}`);
-    })
+if (process.env.NODE_ENV !== "production") {
+    console.log("Running in development mode");
 }
+
 // Export Server for vercel
 export default server;
